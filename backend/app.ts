11cae1ef165df8020cf490c0fa0b474b32b12859
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-08-25 16:14:28 +0200

    Change to using Router

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,13 @@
-import express from 'express'
+import express from 'express';
+import router from './src/routes/router';
+import * as dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ test: "hello" });
-})
+app.use(express.json()); // ? parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // ? allow req.body
 
+app.use('/', router);
 app.listen(3000, () => {
   console.log('Listening on Port 3000');
-})
+});
