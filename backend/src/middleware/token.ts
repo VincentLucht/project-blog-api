48cf33d88f855bd48b160e5b0083ca078bd73f74
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-08-25 16:17:03 +0200

    Create JWT class that can extract and verify JWT's

---
+++ backend/src/middleware/token.ts
@@ -0,0 +1,59 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+// ? remove error from req.token
+import 'express';
+declare module 'express' {
+  export interface Request {
+    token?: string;
+  }
+}
+interface jwtRequest extends Request {
+  authData?: any;
+}
+
+class Token {
+  private secretKey: string;
+
+  constructor() {
+    this.secretKey = process.env.SECRET_KEY || '';
+    if (!this.secretKey) {
+      throw new Error('SECRET_KEY is not defined in environment variables');
+    }
+
+    this.verify = this.verify.bind(this);
+  }
+
+  extract(req: Request, res: Response, next: NextFunction) {
+    // get authorization header value
+    const bearerHeader = req.headers['authorization'];
+    if (bearerHeader) {
+      const bearer = bearerHeader.split(' ');
+      const [, token] = bearer;
+      // set the token
+      req.token = token;
+      next();
+    } else {
+      res.sendStatus(403);
+    }
+  }
+
+  verify(req: jwtRequest, res: Response, next: NextFunction) {
+    const { token } = req;
+    if (!token) {
+      return res.status(401).json({ error: 'No token provided' });
+    }
+
+    jwt.verify(token, this.secretKey, (error: Error | null, authData) => {
+      if (error) {
+        res.sendStatus(403);
+      } else {
+        req.authData = authData; // Attach authData to the request object
+        next();
+      }
+    });
+  }
+}
+
+const token = new Token();
+export default token;
