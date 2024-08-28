a289d9075df947482ae940b0a76d1068515be05d
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-08-28 14:18:40 +0200

    Add rules for creating a blog and logging in

--- backend/src/auth/validation.ts
+++ backend/src/auth/validation.ts
@@ -23,6 +23,39 @@ class Validator {
         .withMessage('Role must either be BASIC or AUTHOR'),
     ];
   }
+
+  createBlogRules() {
+    return [
+      body('title')
+        .trim()
+        .notEmpty()
+        .withMessage('Title is required')
+        .isLength({ max: 50 })
+        .withMessage('Title must be at most 50 characters long'),
+
+      body('content')
+        .trim()
+        .notEmpty()
+        .withMessage('Content is required')
+        .isLength({ max: 50000 })
+        .withMessage('Content must be at most 50000 characters long'),
+
+      body('is_published')
+        .isBoolean()
+        .withMessage('Published must be a boolean value'),
+    ];
+  }
+
+  loginRules() {
+    return [
+      body('name').trim()
+        .notEmpty()
+        .withMessage('Name is required'),
+      body('password').trim()
+        .notEmpty()
+        .withMessage('Password is required'),
+    ];
+  }
 }
 
 export const validator = new Validator();
