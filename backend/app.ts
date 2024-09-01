cdb1b6540c5829b1cb6786becd5d9e998e783e7e
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-01 16:48:02 +0200

    Add CORS globally

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 import router from './src/routes/router';
 import * as dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 const app = express();
+
+// Configure CORS globally
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }),
+);
 
 app.use(express.json()); // ? parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // ? allow req.body
