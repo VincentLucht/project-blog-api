31dc572cecb23d56718564645b6bba49645370f3
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-01 16:50:18 +0200

    Initialize App with context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,38 @@
 import './css/App.css';
+import { Route, Routes } from 'react-router-dom';
+import { AuthContextProvider } from './components/auth/AuthContextProvider';
+
+import Blogs from './components/blogs/Blogs';
+import Header from './components/header/Header';
+import Login from './components/header/login/Login';
+import Account from './components/account/Account';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/ReactToastify.css';
+
+export const API_URL = 'http://localhost:3000';
 
 export default function App() {
-  return <div>I&apos;m a template</div>;
+  return (
+    <AuthContextProvider>
+      <div className="flex min-h-screen flex-col">
+        <header>
+          <Header />
+        </header>
+
+        <main className="flex flex-grow items-center justify-center">
+          <Routes>
+            <Route path="/" element={<Blogs />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="*" element="Not found" />
+          </Routes>
+        </main>
+
+        <footer></footer>
+
+        <ToastContainer theme="dark" />
+      </div>
+    </AuthContextProvider>
+  );
 }
