e4ff1da6c45f26dffb4f35e0816cf0d55b0d8a11
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-03 12:06:25 +0200

    Add connection error component

---
+++ frontend/src/components/partials/ConnectionError.tsx
@@ -0,0 +1,17 @@
+function ConnectionError() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <img src="cat503.webp" alt="" className="mb-4 w-full max-w-[684px]" />
+
+      <h1 className="mb-4 text-4xl font-bold text-white">503 - Service Unavailable</h1>
+      <p className="text-lg text-gray-300">
+        <span className="mb-2 block">
+          Oops! It seems there&apos;s a problem with the connection.
+        </span>
+        Please try refreshing the page or check your internet connection.
+      </p>
+    </div>
+  );
+}
+
+export default ConnectionError;
