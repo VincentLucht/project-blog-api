5812468dddb78b426431aae1f4abd1fe024d0cb3
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-05 13:47:11 +0200

    Add new routes for creation and viewing of blogs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
 import './css/App.css';
+import 'react-toastify/ReactToastify.css';
 import { Route, Routes } from 'react-router-dom';
 import { AuthContextProvider } from './components/auth/AuthContextProvider';
 
-import Blogs from './components/blogs/Blogs';
 import Header from './components/header/Header';
+
+import Blogs from './components/blogs/Blogs';
+import { BlogDetail } from './components/blogs/BlogDetail/BlogDetail';
+
 import Login from './components/header/login/Login';
 import Account from './components/account/Account';
 
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/ReactToastify.css';
 import NotFound from './components/partials/NotFound';
+import BlogHub from './components/blogs/CreateBlog/BlogHub';
+import CreateBlog from './components/blogs/CreateBlog/CreateBlog';
 
 export const API_URL = 'http://localhost:3000';
 
@@ -24,6 +29,9 @@ export default function App() {
         <main className="flex flex-grow items-center justify-center">
           <Routes>
             <Route path="/" element={<Blogs />} />
+            <Route path="/:id" element={<BlogDetail />} />
+            <Route path="/hub" element={<BlogHub />} />
+            <Route path="/hub/:id" element={<CreateBlog />} />
             <Route path="/login" element={<Login />} />
             <Route path="/account" element={<Account />} />
             <Route path="*" element={<NotFound />} />
