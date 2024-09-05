b32f9e9f414fb90fa92a65a7d489afba07fd6b0b
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-05 13:56:49 +0200

    Add "/" to point to public folder correctly

--- frontend/src/components/partials/ConnectionError.tsx
+++ frontend/src/components/partials/ConnectionError.tsx
@@ -1,7 +1,7 @@
 function ConnectionError() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <img src="cat503.webp" alt="" className="mb-4 w-full max-w-[684px]" />
+      <img src="/cat503.webp" alt="" className="mb-4 w-full max-w-[684px]" />
 
       <h1 className="mb-4 text-4xl font-bold text-white">503 - Service Unavailable</h1>
       <p className="text-lg text-gray-300">
