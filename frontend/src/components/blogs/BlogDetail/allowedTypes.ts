c496c69aca3b471e5538d487e1b1b6e82053f0b6
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-10 16:15:51 +0200

    Add more content types

---
+++ frontend/src/components/blogs/BlogDetail/allowedTypes.ts
@@ -0,0 +1,9 @@
+export const allowedTypes = [
+  'large header',
+  'header',
+  'small header',
+  'text',
+  'image',
+  'line break',
+  'code block',
+];
--- frontend/src/components/blogs/CreateBlog/getPlaceholder.ts
+++ frontend/src/components/blogs/CreateBlog/getPlaceholder.ts
@@ -2,12 +2,23 @@ import { ContentTypes } from '../BlogDetail/BlogDetail';
 
 export function getPlaceholder(type: ContentTypes) {
   switch (type) {
+    case 'large header':
+    case 'header':
+    case 'small header':
+      return 'Enter header';
+
     case 'text':
       return 'Enter text';
-    case 'header':
-      return 'Enter header';
+
     case 'image':
       return 'Enter image URL';
+
+    case 'line break':
+      return 'Add line break';
+
+    case 'code block':
+      return 'Add code block';
+
     default:
       return 'Enter value';
   }
