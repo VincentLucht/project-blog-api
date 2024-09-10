8afa36e4ab8a0ff4d4c5c794fc232ca6a6edb512
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-10 16:08:14 +0200

    Add utility classes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -32,11 +32,33 @@ export default {
         '.clamp-sm': {
           width: 'clamp(300px, 30vw, 400px)',
         },
+        '.clamp-ss': {
+          width: 'clamp(100px, 15vw, 300px)',
+        },
+        '.edit-textarea': {
+          width: 'clamp(100px, 25vw, 350px)',
+          borderRadius: '1.5rem',
+          borderWidth: '2px',
+          borderColor: 'transparent',
+          paddingLeft: '1.25rem',
+          paddingRight: '2.75rem',
+          paddingTop: '0.75rem',
+          paddingBottom: '0.75rem',
+          '&:hover': {
+            borderColor: 'white',
+          },
+          '&:focus': {
+            borderColor: 'transparent',
+          },
+        },
         '.clamp-card': {
           width: 'clamp(400px, 40vw, 800px)',
         },
         '.calc-h-vw': {
           height: 'calc(100vh - 64px)',
+        },
+        '.calc-h-vw-1': {
+          height: 'calc(100vh - 110px)',
         },
         '.box-shadow-br': {
           boxShadow: '10px 10px 2px 1px rgba(0, 0, 0, 0.15)',
