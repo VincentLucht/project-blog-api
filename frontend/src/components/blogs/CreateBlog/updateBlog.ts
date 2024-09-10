fe1dd6d2d3347d0df4371f62882624203401eb68
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-10 16:17:08 +0200

    Implement checking if the user is allowed to edit the blog, add error message

--- frontend/src/components/blogs/CreateBlog/updateBlog.ts
+++ frontend/src/components/blogs/CreateBlog/updateBlog.ts
@@ -1,14 +1,25 @@
 import { CompleteBlogItem } from '../BlogDetail/BlogDetail';
+import { isAllowedToEdit } from './isAllowedToEdit';
 import { API_URL } from '../../../App';
 import { toast } from 'react-toastify';
 import { ValidationError } from '../../header/login/Login';
 
+import { User } from '../../account/useGetToken';
+import { jwtDecode } from 'jwt-decode';
+
 interface ErrorResponse {
   errors: ValidationError[];
+  error?: string;
 }
 
 export async function updateBlog(updatedBlogItem: CompleteBlogItem, token: string) {
   const { id, title, summary, is_published, updated_at, content } = updatedBlogItem;
+  const user: User = jwtDecode(token);
+
+  if (!(await isAllowedToEdit(user.id, id, token))) {
+    toast.error('This is not your blog, please go away!');
+    throw new Error('Forbidden');
+  }
 
   const response = await fetch(`${API_URL}/blogs/${id}`, {
     method: 'PUT',
@@ -26,15 +37,22 @@ export async function updateBlog(updatedBlogItem: CompleteBlogItem, token: strin
   });
   if (!response.ok) {
     const errorData = (await response.json()) as ErrorResponse;
+
     if (errorData.errors) {
       // express-validator errors
       errorData.errors.map((error: ValidationError) => {
         toast.error(error.msg, { autoClose: 10000 });
       });
       throw new Error('There was an error while updating the Blog');
+    } else if (errorData.error && errorData.error === 'Forbidden') {
+      const errorMessage = 'This is not your blog, please go away!';
+      toast.error(errorMessage, { autoClose: 10000 });
+      throw new Error(errorMessage);
     } else {
-      // other error
-      throw new Error('There was an error while updating the Blog');
+      // fallback
+      const errorMessage = 'An unknown error occurred while updating the blog.';
+      toast.error(errorMessage, { autoClose: 10000 });
+      throw new Error(errorMessage);
     }
   }
 }
