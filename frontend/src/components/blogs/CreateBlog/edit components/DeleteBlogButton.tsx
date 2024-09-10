3cd7b069e78c1b86c789fa48a95e90320b04c30e
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-10 19:12:24 +0200

    Add DeleteBlockButton component that allows for deletion of a blog

---
+++ frontend/src/components/blogs/CreateBlog/edit components/DeleteBlogButton.tsx
@@ -0,0 +1,41 @@
+import { useNavigate } from 'react-router-dom';
+import { deleteBlog } from '../deleteBlog';
+import { toast } from 'react-toastify';
+
+interface DeleteBlockButtonProps {
+  blogId: string;
+  token: string;
+}
+
+function DeleteBlockButton({ blogId, token }: DeleteBlockButtonProps) {
+  const navigate = useNavigate();
+
+  const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    const areYouSure = confirm('Are you sure you want to delete this Blog?');
+    const areYouReallySure = confirm('WARNING! This action is irreversible.');
+
+    if (areYouSure && areYouReallySure) {
+      deleteBlog(blogId, token)
+        .then(() => {
+          toast.success('Successfully deleted Blog');
+          navigate('/hub');
+        })
+        .catch(() => {
+          toast.info('Your Blog was not deleted', { autoClose: 15000 });
+          toast.error('There was an error while deleting the Blog', {
+            autoClose: 12500,
+          });
+        });
+    }
+  };
+
+  return (
+    <button className="border p-4" onClick={(e) => handleDelete(e)}>
+      Delete Blog
+    </button>
+  );
+}
+
+export default DeleteBlockButton;
