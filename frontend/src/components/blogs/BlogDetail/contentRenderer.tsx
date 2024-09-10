06d0ffb26c91b44ecbb82dd4a2cb9fc2e168cb2b
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-10 16:14:52 +0200

    Rework into proper JSX component, due to needing to use a hook for prismJS

--- frontend/src/components/blogs/BlogDetail/contentRenderer.tsx
+++ frontend/src/components/blogs/BlogDetail/contentRenderer.tsx
@@ -1,35 +1,82 @@
 import { Content } from './BlogDetail';
+import { Reorder } from 'framer-motion';
 
-function contentRenderer(
-  content: Content[],
-  isCreationMode: boolean,
-  onDeleteBlock?: (index: number) => void,
-) {
-  const renderedContent = content.map((content, index) => {
-    return (
-      <div key={index} className="relative">
-        {content.type === 'header' && (
-          <h2 className="text-left h2">{content.content}</h2>
-        )}
-        {content.type === 'image' && (
-          <img src={`${content.content}`} alt={`${content.content}`} />
-        )}
-        {content.type === 'text' && <p className="text-left">{content.content}</p>}
+import Prism from 'prismjs';
+import { useEffect } from 'react';
+import '../../../css/prism.css';
+import 'prismjs/themes/prism-twilight.css';
 
-        {/* Add delete button in creation mode */}
-        {isCreationMode && (
-          <button
-            onClick={() => onDeleteBlock?.(index)}
-            className="absolute right-0 top-0 p-2 text-red-600"
-          >
-            Delete
-          </button>
+interface ContentRendererInterface {
+  content: Content[];
+  isCreationMode?: boolean;
+  onDeleteBlock?: (id: string) => void;
+}
+
+function ContentRenderer({
+  content,
+  isCreationMode,
+  onDeleteBlock,
+}: ContentRendererInterface) {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [content]);
+
+  const renderedContent = content.map((contentItem, index) => {
+    contentItem.order = index + 1;
+
+    const contentElement = (
+      <div className="grid grid-cols-[90%_10%] gap-2">
+        <div>
+          {contentItem.type === 'large header' && (
+            <h1 className="text-left text-5xl font-black">{contentItem.content}</h1>
+          )}
+
+          {contentItem.type === 'header' && (
+            <h2 className="text-left h2">{contentItem.content}</h2>
+          )}
+
+          {contentItem.type === 'small header' && (
+            <h1 className="text-left text-2xl font-bold">{contentItem.content}</h1>
+          )}
+
+          {contentItem.type === 'text' && (
+            <div className="whitespace-pre-wrap text-left">{contentItem.content}</div>
+          )}
+
+          {contentItem.type === 'image' && (
+            <img src={`${contentItem.content}`} alt={`${contentItem.content}`} />
+          )}
+
+          {contentItem.type === 'line break' && <br />}
+
+          {contentItem.type === 'code block' && (
+            <pre className="flex justify-start">
+              <code className="language-javascript">{contentItem.content}</code>
+            </pre>
+          )}
+        </div>
+
+        {isCreationMode && onDeleteBlock && (
+          <div className="flex content-center justify-around">
+            <div className="w-7 df">{contentItem.order}</div>
+            <button onClick={() => onDeleteBlock(contentItem.id)}>
+              <img src="/close.svg" alt="close" className="h-8 w-8" />
+            </button>
+          </div>
         )}
       </div>
     );
+
+    return isCreationMode ? (
+      <Reorder.Item className="mr-4" key={contentItem.id} value={contentItem}>
+        {contentElement}
+      </Reorder.Item>
+    ) : (
+      <div key={contentItem.id}>{contentElement}</div>
+    );
   });
 
-  return renderedContent;
+  return <div>{renderedContent}</div>;
 }
 
-export default contentRenderer;
+export default ContentRenderer;
