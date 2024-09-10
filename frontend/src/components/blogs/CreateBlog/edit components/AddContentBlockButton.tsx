69299d7052ed8b3e590c0f7b3f74f1a6b287b4fe
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-10 16:13:49 +0200

    Separate the add content blog functionality into its own component

---
+++ frontend/src/components/blogs/CreateBlog/edit components/AddContentBlockButton.tsx
@@ -0,0 +1,60 @@
+import { v4 as uuid } from 'uuid';
+import { toast } from 'react-toastify';
+
+import { Content } from '../../BlogDetail/BlogDetail';
+import { ContentTypes } from '../../BlogDetail/BlogDetail';
+import { allowedTypes } from '../../BlogDetail/allowedTypes';
+
+interface AddContentBlockButtonProps {
+  selectedType: ContentTypes;
+  content: string;
+  blocks: Content[];
+  setBlocks: (newBlocks: Content[]) => void;
+  id: string;
+}
+
+function AddContentBlockButton({
+  selectedType,
+  content,
+  blocks,
+  setBlocks,
+  id,
+}: AddContentBlockButtonProps) {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (content === '' && selectedType !== 'line break') return;
+    if (blocks.length === 500) {
+      toast.warn(
+        'You have reached 500 blocks, which is half of the limit of 1000 blocks',
+      );
+    }
+    if (blocks.length >= 1000) {
+      toast.error('You have reached the limit of 1000 blocks', { autoClose: false });
+      toast.info('Please try to optimize by removing unnecessary blocks', {
+        autoClose: false,
+      });
+      return;
+    }
+
+    if (allowedTypes.includes(selectedType)) {
+      const newBlock: Content = {
+        id: uuid(),
+        type: selectedType,
+        content,
+        order: blocks.length + 1,
+        blogId: id,
+      };
+      setBlocks([...blocks, newBlock]);
+    } else {
+      toast.error("Don't change this! 😡");
+    }
+  };
+
+  return (
+    <button className="border p-4" onClick={handleClick}>
+      Add Content Block
+    </button>
+  );
+}
+
+export default AddContentBlockButton;
