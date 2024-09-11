8daa9ab845a0a1b785ede96c72a0b5d4b6679893
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-11 14:49:27 +0200

    Remove unnecessary userId, as it is checked on the server side

--- frontend/src/components/blogs/CreateBlog/createBlog.ts
+++ frontend/src/components/blogs/CreateBlog/createBlog.ts
@@ -1,12 +1,11 @@
 import { API_URL } from '../../../App';
 
-export async function createBlog(token: string, userId: string) {
+export async function createBlog(token: string) {
   const response = await fetch(`${API_URL}/blogs`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Authorization: token,
-      body: JSON.stringify({ userId }),
     },
   });
 
