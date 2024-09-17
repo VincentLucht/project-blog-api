211c49fcd19fd19e7cb7b1ec82f6119738b7d033
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-17 13:59:42 +0200

    Add methods for searching, getting comments, deleting blogs, and add adding and replying to comments

--- backend/src/controllers/controller.ts
+++ backend/src/controllers/controller.ts
@@ -61,6 +61,15 @@ class BlogController {
     this.createBlog = this.createBlog.bind(this);
   }
 
+  searchBlog = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
+    const { title } = req.body;
+    console.log(title);
+    if (!title) return res.status(404).json({ error: 'Not found' });
+
+    const foundBlogs = await db.searchBlog(title);
+    return res.json({ foundBlogs });
+  });
+
   getAllBlogs = asyncHandler(async (req: Request, res: Response) => {
     const allBlogs = await db.getAllBlogs();
     return res.json({ data: allBlogs });
@@ -77,10 +86,16 @@ class BlogController {
     return res.json({ data: allBlogs });
   });
 
-  getBlogWithComments = asyncHandler(async (req: Request, res: Response) => {
+  getBlog = asyncHandler(async (req: Request, res: Response) => {
     const blogId = req.params.id;
-    const blog = await db.getBlogWithComments(blogId);
+    const blog = await db.getBlog(blogId);
     return res.json({ data: blog });
+  });
+
+  getBlogComments = asyncHandler(async (req: Request, res: Response) => {
+    const blogId = req.params.id;
+    const blogComments = await db.getBlogComments(blogId);
+    return res.json({ blogComments });
   });
 
   createBlog = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
@@ -89,26 +104,24 @@ class BlogController {
     if (!user) return res.status(401).json({ message: 'Unauthorized' });
     if (checkValidationError(req, res)) return;
 
-    const { title, content, is_published, content_blogs } = req.body;
-
     const userId = user.id;
-    const blog = await db.createBlog(userId, title, content, is_published, content_blogs);
+    const blog = await db.createBlog(userId);
 
     return res.json({ message: 'Blog created successfully', user, blog });
   });
 
   updateBlog = asyncHandler(async (req: Request, res: Response) => {
-    if (checkValidationError(req, res)) return;
-
     const user = req.user as User | undefined;
     if (!user) return res.status(401).json({ message: 'You are not logged in!' });
-
-    //  check if user is allowed to edit blog
-    if (user.role !== 'AUTHOR') return res.status(403).json({ error: 'Access denied, you are not an author' });
 
     // check if blog exists and if user is author
     const blogId = req.params.id;
     if (!(await db.isInBlogAuthors(user.id, blogId))) return res.status(403).json({ error: 'Forbidden' });
+
+    if (checkValidationError(req, res)) return;
+
+    //  check if user is allowed to edit blog
+    if (user.role !== 'AUTHOR') return res.status(403).json({ error: 'Access denied, you are not an author' });
 
     // update the blog
     const { title, summary, is_published, updated_at, content } = req.body;
@@ -116,7 +129,81 @@ class BlogController {
 
     return res.json({ message: 'Blog update successful' });
   });
+
+  deleteBlog = asyncHandler(async (req: Request, res: Response) => {
+    const user = req.user as User | undefined;
+    if (!user) return res.status(401).json({ message: 'You are not logged in!' });
+
+    const blogId = req.params.id;
+    if (!(await db.isInBlogAuthors(user.id, blogId))) return res.status(403).json({ error: 'Forbidden' });
+
+    await db.deleteBlog(blogId);
+
+    return res.json({ message: 'Successfully deleted Blog' });
+  });
+
+  isAllowedToEdit = asyncHandler(async (req: Request, res: Response) => {
+    const { userId, blogId } = req.params;
+    const isAllowed = await db.isInBlogAuthors(userId, blogId);
+    return res.json({ isAllowed });
+  });
+}
+
+class CommentController {
+  private secretKey: string;
+
+  constructor() {
+    this.secretKey = process.env.SECRET_KEY || '';
+    if (!this.secretKey) {
+      throw new Error('SECRET_KEY is not defined in environment variables');
+    }
+  }
+
+  addComment = asyncHandler(async (req: Request, res: Response) => {
+    const user = req.user as User;
+    if (!user) return res.status(401).json({ message: 'You are not logged in!' });
+
+    if (checkValidationError(req, res)) return;
+
+    const blogId = req.params.id;
+    const { text } = req.body;
+
+    await db.addComment(user.id, blogId, text);
+
+    return res.json({ success: true, message: 'Successfully created Comment' });
+  });
+
+  replyToComment = asyncHandler(async (req: Request, res: Response) => {
+    const user = req.user as User;
+    if (!user) return res.status(401).json({ message: 'You are not logged in' });
+    console.log('Replied to comment');
+
+    if (checkValidationError(req, res)) return;
+
+    const blogId = req.params.id;
+    const { text, parent_comment_id, replied_to_name } = req.body;
+
+    try {
+      await db.replyToComment(blogId, text, parent_comment_id, user.id, replied_to_name);
+      return res.json({ success: true, message: 'Successfully replied to a comment' });
+    } catch (error: any) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        // Check for the specific error code for foreign key constraint failure
+        if (error.code === 'P2003') {
+          return res.status(400).json({
+            success: false,
+            message: 'Failed to reply to comment due to invalid blogId or parent_comment_id',
+          });
+        }
+      }
+      return res.status(500).json({
+        success: false,
+        message: 'An unexpected error occurred',
+      });
+    }
+  });
 }
 
 export const userController = new UserController();
 export const blogController = new BlogController();
+export const commentController = new CommentController();
