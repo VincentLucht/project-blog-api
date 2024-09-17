089258c1bd6954efeb64b105d27b07faa47fbd06
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-17 14:12:16 +0200

    Add user's names to the front page

--- frontend/src/components/blogs/Blog.tsx
+++ frontend/src/components/blogs/Blog.tsx
@@ -1,6 +1,13 @@
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 
+export interface User {
+  user: {
+    id: string;
+    name: string;
+  };
+}
+
 export interface BlogInterface {
   id: string;
   title: string;
@@ -8,6 +15,7 @@ export interface BlogInterface {
   posted_on: Date;
   readMore?: boolean;
   editMode?: boolean;
+  users?: User[];
 }
 
 export function Blog({
@@ -17,31 +25,45 @@ export function Blog({
   posted_on,
   readMore = true,
   editMode = false,
+  users,
 }: BlogInterface) {
   return (
-    <div className="border border-gray-400 p-5 text-left box-shadow-br">
-      <div className="mb-[10px]">
+    <div
+      className="border border-gray-400 p-5 text-left transition-all duration-200 box-shadow-br
+        hover:border-blue-400"
+    >
+      <div>
         <Link to={editMode ? `/hub/${id}` : `/${id}`}>
           <h3 className="mb-[2px] break-words text-3xl font-bold hover:underline">
             {title}
           </h3>
         </Link>
-
-        <div className="text-sm text-gray-400">
-          {dayjs(posted_on).format('D MMM YYYY')}
-        </div>
       </div>
 
-      <div className="text-gray-300">{summary}</div>
-
-      {readMore && (
-        <div className="mt-4">
-          {/* TODO: Add link to blog too */}
-          <Link to={`/${id}`} className="text-blue-400 underline underline-offset-2">
-            Read more
-          </Link>
+      <div className="pl-1">
+        <div className="mb-[10px] flex items-baseline gap-1">
+          {users?.map((user, index) => (
+            <div className="text-base leading-none" key={index}>
+              <span>{user.user.name}</span>
+              {index !== users.length - 1 && ','}
+            </div>
+          ))}
+          <span className="flex items-center text-sm leading-none text-gray-400">
+            on {dayjs(posted_on).format('D MMM, YYYY')}
+          </span>
         </div>
-      )}
+
+        <div className="text-gray-300">{summary}</div>
+
+        {readMore && (
+          <div className="mt-4">
+            {/* TODO: Add link to blog too */}
+            <Link to={`/${id}`} className="text-blue-400 underline underline-offset-2">
+              Read more
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
