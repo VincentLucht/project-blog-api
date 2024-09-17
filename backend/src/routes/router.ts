a02496896ffff73ccd5dcad5fc21d5b62b4cd991
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-17 14:01:39 +0200

    Add routers to get and add comments

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { userController, blogController } from '../controllers/controller';
+import { userController, blogController, commentController } from '../controllers/controller';
 import { validator } from '../auth/validation';
 import authController from '../controllers/authController';
 import token from '../middleware/token';
@@ -8,26 +8,39 @@ const router = express.Router();
 
 // CREATE
 router.post('/sign-up', validator.signUpRules(), userController.createUser);
+router.post('/blogs', token.extract, token.verify, blogController.createBlog);
 
-router.post('/blogs', token.extract, token.verify, validator.blogRules(), blogController.createBlog);
+router.post(
+  '/blogs/:id/comments',
+  token.extract,
+  token.verify,
+  validator.addCommentRules(),
+  commentController.addComment,
+);
+router.post(
+  '/blogs/:id/comments/reply',
+  token.extract,
+  token.verify,
+  validator.replyToCommentRules(),
+  commentController.replyToComment,
+);
 
 // READ
 router.get('/users', token.extract, token.verify, userController.getAllUsers);
 router.get('/users/:id', token.extract, token.verify, userController.getUser);
 router.get('/users/blogs/:id', token.extract, token.verify, blogController.getAllBlogsFromUser);
+router.post('/blogs/:name', blogController.searchBlog);
 
 router.get('/blogs', blogController.getAllBlogsWithUsers);
-router.get('/blogs/:id', blogController.getBlogWithComments);
+router.get('/blogs/:id', blogController.getBlog);
+router.get('/blogs/:id/comments', blogController.getBlogComments);
 
 // UPDATE
-// TODO: update blogs => for other front end
 router.put('/blogs/:id', token.extract, token.verify, validator.blogRules(), blogController.updateBlog);
-
-// TODO update comments??
+router.get('/blogs/:userId/:blogId', token.extract, token.verify, blogController.isAllowedToEdit);
 
 // DELETE
-// TODO: delete blogs => for other front end
-// TODO delete comments
+router.delete('/blogs/:id', token.extract, token.verify, blogController.deleteBlog);
 
 // LOGGING IN
 router.post('/login', validator.loginRules(), authController.logIn);
