9d4b5301090ce327e551ec89403fad0e2d57d280
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-29 00:25:01 +0200

    Add optional second message

--- frontend/src/components/partials/noMessageFound.tsx
+++ frontend/src/components/partials/noMessageFound.tsx
@@ -1,7 +1,17 @@
-function NoMessageFound({ message }: { message: string }) {
+interface NoMessageFoundProps {
+  message: string;
+  secondMessage?: string;
+}
+
+function NoMessageFound({ message, secondMessage }: NoMessageFoundProps) {
   return (
     <div className="flex-col df">
       <div className="mt-10 text-lg font-semibold">{message}</div>
+
+      {secondMessage && (
+        <div className="mt-10 text-lg font-semibold">{secondMessage}</div>
+      )}
+
       <img
         className="w-96"
         src="./catNothingFound.gif"
