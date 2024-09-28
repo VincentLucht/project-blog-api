525b56a4df2126432b7381612704613ca0474d4f
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-29 00:27:58 +0200

    Add searchbar, and fix and add various things
    - add no Blogs found gif
    - flatten data structure to allow easier access (e.g. blog.id instead blog.blog.id)
    - add proper loading screen

--- frontend/src/components/blogs/CreateBlog/BlogHub.tsx
+++ frontend/src/components/blogs/CreateBlog/BlogHub.tsx
@@ -4,21 +4,29 @@ import { useGetUser } from '../../account/useGetToken';
 import { useGetToken } from '../../account/useGetToken';
 
 import CreateBlogButton from './CreateBlogButton';
-import { Blog, BlogInterface } from '../Blog';
-import fetchUserBlogs from './fetchUserBlogs';
+import SearchBar from '../SearchBar';
+import { SortOptions } from '../SortOptions';
+import FilterOptions from '../FilterOptions';
+import { Blog } from '../Blog';
+import { BlogTags } from '../BlogDetail/BlogDetail';
+import fetchUserBlogs, { FetchUserBlogsResponse } from './util/fetchUserBlogs';
 
 import NotLoggedIn from '../../partials/NotLoggedIn';
 import ConnectionError from '../../partials/ConnectionError';
+import NoMessageFound from '../../partials/noMessageFound';
+import Loading from '../../partials/Loading';
 
-export interface AllUserBlogs {
+export interface BlogData {
   id: string;
-  blog_id: string;
-  use_id: string;
-  blog: BlogInterface;
+  summary: string;
+  tags: BlogTags[];
+  title: string;
+  updated_at: Date;
 }
 
 function BlogHub() {
-  const [userBlogs, setUserBlogs] = useState<AllUserBlogs[] | null>(null);
+  const [userBlogs, setUserBlogs] = useState<BlogData[]>([]);
+  const [originalUserBlogs, setOriginalUserBlogs] = useState<BlogData[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [isConnectionError, setConnectionError] = useState<boolean>(false);
@@ -31,11 +39,12 @@ function BlogHub() {
     if (user?.id && token) {
       setLoading(true);
       fetchUserBlogs(user.id, token)
-        .then((result) => {
-          if (!result.data) {
+        .then((result: FetchUserBlogsResponse) => {
+          if (!result) {
             logout();
           }
-          setUserBlogs(result.data.blogs);
+          setUserBlogs(result.allBlogs);
+          setOriginalUserBlogs(result.allBlogs);
           setLoading(false);
         })
         .catch((e) => {
@@ -51,13 +60,13 @@ function BlogHub() {
   }, [user?.id, token, logout]);
 
   if (!user) return <NotLoggedIn />;
-  if (loading) return <div>Loading...</div>;
+  if (loading) return <Loading />;
   if (error) return <div>Error: {error}</div>;
   if (isConnectionError) return <ConnectionError />;
-  if (!userBlogs) return <div>No user data available.</div>;
+  if (!userBlogs) return <NoMessageFound message="No user data available..." />;
 
   return (
-    <div>
+    <>
       <div className="mb-4 flex justify-between">
         <h2 className="text-left h2">These are your blogs:</h2>
         <div className="df">
@@ -65,31 +74,39 @@ function BlogHub() {
         </div>
       </div>
 
+      <div className="mb-8">
+        <div className="mb-4 mt-8 gap-5 df">
+          <SearchBar
+            mode="user"
+            setUserBlogs={setUserBlogs}
+            originalUserBlogs={originalUserBlogs}
+          />
+
+          <SortOptions blogs={userBlogs} setBlogs={setUserBlogs} />
+        </div>
+
+        <FilterOptions originalData={originalUserBlogs} setBlogs={setUserBlogs} />
+      </div>
+
       {userBlogs.length > 0 ? (
         <div className="grid gap-8">
           {userBlogs.map((blog, index) => (
             <Blog
               key={index}
-              id={blog.blog.id}
-              title={blog.blog.title}
-              posted_on={blog.blog.posted_on}
-              summary={blog.blog.summary}
+              id={blog.id}
+              title={blog.title}
+              posted_on={blog.updated_at}
+              summary={blog.summary}
+              tags={blog.tags}
               readMore={false}
               editMode={true}
             />
           ))}
         </div>
       ) : (
-        <div className="flex-col df">
-          <div className="mt-10 text-lg font-semibold">No blogs here...</div>
-          <img
-            className="w-96"
-            src="./catNothingFound.gif"
-            alt="gif of a cat poking a knitting ball"
-          />
-        </div>
+        <NoMessageFound message="No Blogs found here..." />
       )}
-    </div>
+    </>
   );
 }
 
