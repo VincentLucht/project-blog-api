26ee9c311965419ccf844b929d473aed471e48e3
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-29 00:25:48 +0200

    Remove content types

--- frontend/src/components/blogs/CreateBlog/edit components/AddContentBlockButton.tsx
+++ frontend/src/components/blogs/CreateBlog/edit components/AddContentBlockButton.tsx
@@ -2,27 +2,24 @@ import { v4 as uuid } from 'uuid';
 import { toast } from 'react-toastify';
 
 import { Content } from '../../BlogDetail/BlogDetail';
-import { ContentTypes } from '../../BlogDetail/BlogDetail';
-import { allowedTypes } from '../../BlogDetail/allowedTypes';
 
 interface AddContentBlockButtonProps {
-  selectedType: ContentTypes;
   content: string;
+  setContent: (newValue: string) => void;
   blocks: Content[];
   setBlocks: (newBlocks: Content[]) => void;
   id: string;
 }
 
 function AddContentBlockButton({
-  selectedType,
   content,
+  setContent,
   blocks,
   setBlocks,
   id,
 }: AddContentBlockButtonProps) {
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    if (content === '' && selectedType !== 'line break') return;
     if (blocks.length === 500) {
       toast.warn(
         'You have reached 500 blocks, which is half of the limit of 1000 blocks',
@@ -36,22 +33,19 @@ function AddContentBlockButton({
       return;
     }
 
-    if (allowedTypes.includes(selectedType)) {
-      const newBlock: Content = {
-        id: uuid(),
-        type: selectedType,
-        content,
-        order: blocks.length + 1,
-        blogId: id,
-      };
-      setBlocks([...blocks, newBlock]);
-    } else {
-      toast.error("Don't change this! 😡");
-    }
+    const newBlock: Content = {
+      id: uuid(),
+      content,
+      order: blocks.length + 1,
+      blogId: id,
+    };
+
+    setContent('');
+    setBlocks([...blocks, newBlock]);
   };
 
   return (
-    <button className="prm-button h-12 w-full" onClick={handleClick}>
+    <button className="h-12 w-full prm-button" onClick={handleClick}>
       Add Content Block
     </button>
   );
