5a4f075b238fba27c7d34f83b98122bb03889a93
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-29 15:45:28 +0200

    Center login form

--- frontend/src/components/header/login/Login.tsx
+++ frontend/src/components/header/login/Login.tsx
@@ -70,7 +70,7 @@ function Login() {
   };
 
   return (
-    <div className="df calc-h-vw-1">
+    <div className="pb-14 df calc-h-vw-2">
       <div className="flex-col rounded-xl border px-10 pb-12 pt-6 df">
         <h2 className="mb-6 h2">Login</h2>
 
