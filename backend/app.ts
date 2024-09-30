730808bbc36e6dd289d3ff13ab7ed04ab088e3f1
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-09-30 17:28:01 +0200

    Add frontend url to CORS

--- backend/app.ts
+++ backend/app.ts
@@ -8,7 +8,7 @@ const app = express();
 // Configure CORS globally
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }),
 );
